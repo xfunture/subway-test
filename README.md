7ecc6440fb910059821bed83506b64df84aac6c0
xfunture <15768184216@163.com>
2024-01-20 18:39:07 +0800

    Sandwich.sol

--- README.md
+++ README.md
@@ -64,3 +64,9 @@ $ forge --help
 $ anvil --help
 $ cast --help
 ```
+
+
+### deploy Sandwich.sol
+```
+forge create --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 src/Sandwich.sol:Sandwich
+```
--- src/Sandwich.sol
+++ src/Sandwich.sol
@@ -1,15 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+// pragma solidity >=0.5.0 <0.6.0;
+import "./IERC20.sol";
+import "./SafeTransfer.sol";
+
 contract Sandwich{
     uint256 public x;
-    receive() external payable{
+    // using SafeTransfer for IERC20;
 
+    address internal user;
+    bytes4 internal constant ERC20_TRANSFER_ID = 0xa9059cbb;
+    bytes4 internal constant PAIR_SWAP_ID = 0x022c0d9f;
+    
+
+    receive() external payable{}
+
+    constructor(address _owner){
+        user = _owner;
     }
 
-    fallback() external payable{
-        assembly  {
-            let value := calldataload(0x00)
-            sstore(x.slot,value)
-        }
+    function recoverERC20(address token) public {
+        require(msg.sender == user,"shoo");
+        IERC20(token).transfer(
+            msg.sender,
+            IERC20(token).balanceOf(address(this))
+        );
     }
+
+
+    fallback() external payable {}
+
+
+    // fallback() external payable{
+    //     assembly  {
+    //         let value := calldataload(0x00)
+    //         sstore(x.slot,value)
+    //     }
+    // }
 }
