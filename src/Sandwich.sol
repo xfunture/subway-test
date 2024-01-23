b7c2a3324f56594f851fc3d9911986bc9b76c666
xfunture <15768184216@163.com>
2024-01-23 15:00:24 +0800

    SWAP

--- src/Sandwich.sol
+++ src/Sandwich.sol
@@ -1,40 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-// pragma solidity >=0.5.0 <0.6.0;
 import "./IERC20.sol";
 import "./SafeTransfer.sol";
 
-contract Sandwich{
-    uint256 public x;
-    // using SafeTransfer for IERC20;
+interface IUniswapV2Pair {
+    function swap(
+        uint amount0Out,
+        uint amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+}
 
-    address internal user;
+contract Sandwich {
+    using SafeTransfer for IERC20;
+
+    address internal immutable user;
     bytes4 internal constant ERC20_TRANSFER_ID = 0xa9059cbb;
     bytes4 internal constant PAIR_SWAP_ID = 0x022c0d9f;
-    
 
-    receive() external payable{}
+    receive() external payable {}
 
-    constructor(address _owner){
+    constructor(address _owner) {
         user = _owner;
     }
 
     function recoverERC20(address token) public {
-        require(msg.sender == user,"shoo");
-        IERC20(token).transfer(
-            msg.sender,
-            IERC20(token).balanceOf(address(this))
-        );
+        // same code here...
     }
 
+    function swap(
+        address token,
+        address pair,
+        uint128 amountIn,
+        uint128 amountOut,
+        uint8 tokenOutNo
+    ) external payable {
+        require(msg.sender == user, "Not the owner");
+        IERC20(token).transfer(pair, amountIn);
+        if (tokenOutNo == 0) {
+            IUniswapV2Pair(pair).swap(amountOut, 0, address(this), "");
+        } else {
+            IUniswapV2Pair(pair).swap(0, amountOut, address(this), "");
+        }
+    }
 
-    fallback() external payable {}
-
-
-    // fallback() external payable{
-    //     assembly  {
-    //         let value := calldataload(0x00)
-    //         sstore(x.slot,value)
-    //     }
-    // }
+    fallback() external payable {
+       // same code here...
+    }
 }
